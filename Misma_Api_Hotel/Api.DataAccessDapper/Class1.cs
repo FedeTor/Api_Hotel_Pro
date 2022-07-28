b906f2eafe8826d0672c156ba1387ad1962eb58d
FedeTor <fedeetorancio@gmail.com>
2022-07-28 17:38:33 -0300

    Common access libreries are added

---
+++ Misma_Api_Hotel/Api.DataAccessDapper/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Api.DataAccessDapper
+{
+    public class Class1
+    {
+    }
+}
---
+++ Misma_Api_Hotel/Api.EntityFrameworkDB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Api.EntityFrameworkDB
+{
+    public class Class1
+    {
+    }
+}
